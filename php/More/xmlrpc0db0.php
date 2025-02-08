7ede99108f42134e76f79b0e9e7a730f6f990510
CodesCamp <how2code546@gmail.com>
2025-02-08 03:34:25 +0000

    Add files via upload

---
+++ php/More/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://www.CodeCamp/</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://www.CodeCamp/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.CodeCamp/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.CodeCamp/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://www.CodeCamp/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.CodeCamp/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
